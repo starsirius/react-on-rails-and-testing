067baefefb63bd9d9619e9f149bc7b4df384aac2
Chung-Yi Chi <chung-yi@artsymail.com>
2016-05-11 22:14:16 -0400

    Use haml, don't use jbuilder, and scaffold a widgets resource; migrate db.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module ReactOnRailsAndTesting
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.jbuilder false
+    end
   end
 end
